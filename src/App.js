b1852a1d4ccc05d904c98ef0ebb703db3c2bae7b
David Andersson <lejonmanen@gmail.com>
2020-05-22 14:14:24 +0200

    Lifting state up demo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import VeggieList from './components/VeggieList';
+import Bordered from './components/Bordered';
+import Login from './components/Login';
+import UserStatus from './components/UserStatus';
 
 function App() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
     return (
         <div className="App">
         <header className="App-header">
-            <h1> Demo </h1>
+            <h1>
+                Demo
+                <Login
+                    isLoggedIn={isLoggedIn}
+                    doLogIn={() => setIsLoggedIn(true)}
+                    doLogOut={() => setIsLoggedIn(false)} />
+            </h1>
         </header>
         <main>
-            <VeggieList />
+            <Bordered>
+                <VeggieList />
+            </Bordered>
+            <Bordered> test </Bordered>
+            <UserStatus isLoggedIn={isLoggedIn} />
         </main>
         </div>
     );
---
+++ src/components/Bordered.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Bordered = ({ children }) => (
+    <div className="nice-border"> {children} </div>
+)
+
+export default Bordered;
